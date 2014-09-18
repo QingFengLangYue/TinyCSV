ea065ac57a65d3465a002839b14e0beb3675fde0
xiaohui_hubei <xiaohui_hubei@163.com>
2014-09-18 12:33:34 +0800

    Signed-off-by: xiaohui_hubei <xiaohui_hubei@163.com>
    create tinycsv class and finish the basic function.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
+#include "tinycsv.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+    /* load and print content */
+    TinyCSV csv("test.csv");
+    if (csv.isValid())
+    {
+        for (int i = 0; i < csv.rowCount(); ++i)
+        {
+            for (int j = 0; j < csv.colCount(); ++j)
+            {
+                cout << csv[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+        /* modify */
+        csv[1][2] = "80";
+        csv[2][2] = std::to_string(std::stoi(csv[2][2]) + 20);  // add 20
+
+        /* save */
+        if (csv.save("test.csv"))
+        {
+            cout << "save ok" << endl;
+        }
+    }
+
+    /* load and read */
+    TinyCSV csv2;
+    if (csv2.load("test.csv"))
+    {
+        // do something...
+    }
+
     return 0;
 }
 
--- tinycsv.h
+++ tinycsv.h
@@ -1,10 +1,49 @@
 #ifndef TINYCSV_H
 #define TINYCSV_H
+#include <string>
+#include <vector>
 
 class TinyCSV
 {
 public:
     TinyCSV();
+    /**
+     * @brief TinyCSV
+     * load csv file specified by "filename".
+     * if load sucessfully isValid() return true,else return false.
+     *
+     * @param filename
+     */
+    TinyCSV(const std::string &filename);
+
+    /**
+     * @brief load
+     * read csv file specified by "filename" and save data in "_data".
+     *
+     * @param filename
+     * csv filename with absolute or relative path.
+     *
+     * @return
+     * true - if load sucessfully, and isValid() return true.
+     * false - load failed, and isValid() return false.
+     */
+    bool load(const std::string &filename);
+    bool save(const std::string &filename, const char delimiter = ',');
+    bool isValid() {return _isValid;}
+    int  rowCount() {return _data.size();}
+    int  colCount() {return _data[0].size();}
+
+    std::vector<std::string> &operator[](int row);
+
+
+protected:
+    bool init();
+
+private:
+    std::vector<std::vector<std::string> > _data;
+    std::string _filename;
+    bool        _isValid;
 };
+
 
 #endif // TINYCSV_H
